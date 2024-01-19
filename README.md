25ff2bfdd931437fb83b16269f556508c07125ea
peter <rorylalor83@gmail.com>
2024-01-20 01:03:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This program is part of a bigger program. 
+It's sole purpose is to provide a quick convenient way to populate categorised dictionaries with themed dishes for use in my buffet generator program. It uses os, json, pprint and system modules. If it has themes_data.json in the directory it will provide an option to open that data or save it. If the .json is not in the directory it will be created.
---
+++ dish_database.py
@@ -0,0 +1,129 @@
+import os
+from pprint import pprint
+import json
+import sys
+# import pdb
+# pdb.set_trace()
+
+
+filename = os.getcwd() + '//' + 'themes_data.json'
+
+themes_dict = {'mexican': {'carb': ['mexican rice'],
+                           'meats': {'beef': ['beef tacos'],
+                                     'chicken': ['chicken quesadilla'],
+                                     'pork': ['carne adovada']},
+                           'seafood': ['ceviche'],
+                           'veg': ['veg fajitas']}}
+
+program_running = True
+
+
+def load_from_file(filename):
+    try:
+        with open(filename, 'r+') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+
+
+def save_to_file(themes_dict, filename):
+    with open(filename, 'a+') as file:
+        json.dump(themes_dict, file)
+
+
+def theme_handler():
+    global theme_name
+
+    if theme_name != 'quit':
+        if theme_name not in themes_dict:
+            themes_dict[theme_name] = {'meats': {}}
+            print(f"{theme_name} added")
+            print('\n')
+        else:
+            print(f"{theme_name} exists")
+            print('\n')
+    else:
+
+        save_to_file(themes_dict, filename)
+        pprint(themes_dict, width=10)
+        print('\n')
+        print(f"Your dictionary has been saved in the current directory as {filename}.")
+        print('\n')
+        sys.exit()
+
+
+def cat_handler():
+    global cat_name
+    global meat_check
+
+    while meat_check not in ['yes', 'no']:
+        print("INVALID CHOICE!")
+        print('\n')
+        meat_check = input("Is this a meat category? YES or NO? >>> ")
+        print('\n')
+
+    if meat_check == "yes":
+        if cat_name not in themes_dict[theme_name]['meats']:
+            themes_dict[theme_name]['meats'][cat_name] = []
+            print(f"{cat_name} added")
+            print('\n')
+        else:
+            print(f"{cat_name} exists")
+            print('\n')
+    if meat_check == 'no':
+        while cat_name not in ['seafood', 'carb', 'veg']:
+            print(f'{cat_name} is an INVALID CHOICE!')
+            print('\n')
+            cat_name = input("Enter the CATEGORY or MEAT >>> ").lower()
+            print('\n')
+        if cat_name not in themes_dict[theme_name]:
+            themes_dict[theme_name][cat_name] = []
+            print(f"{cat_name} added")
+            print('\n')
+        else:
+            print(f'{cat_name} exists')
+            print('\n')
+
+
+def dish_handler():
+    if meat_check == 'yes':
+        if dish_name not in themes_dict[theme_name]['meats'][cat_name]:
+            themes_dict[theme_name]['meats'][cat_name].append(dish_name)
+            print(f"{dish_name} added")
+            print('\n')
+        else:
+            print(f"{dish_name} exists")
+            print('\n')
+    if meat_check == 'no':
+        if dish_name not in themes_dict[theme_name][cat_name]:
+            themes_dict[theme_name][cat_name].append(dish_name)
+            print(f"{dish_name} added")
+            print('\n')
+        else:
+            print(f'{dish_name} exists')
+            print('\n')
+
+
+load_check = input('Do you want to load an existing database from an earlier session (YES / NO)? >>> ')
+print('\n')
+if load_check == 'yes':
+    themes_dict = load_from_file(filename)
+    pprint(themes_dict, width=10)
+    print('\n')
+else:
+    pass
+
+while program_running:
+    theme_name = input("Enter the THEME or type QUIT to exit >>> ").lower()
+    print('\n')
+    theme_handler()
+    cat_name = input("Enter the CATEGORY or MEAT >>> ").lower()
+    print('\n')
+    meat_check = input("Is this a meat category? YES or NO? >>> ")
+    print('\n')
+    cat_handler()
+    dish_name = input("Enter the DISH name >>> ").lower()
+    print('\n')
+    dish_handler()
+    pprint(themes_dict, width=10)
+    print('\n')
